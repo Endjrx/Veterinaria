ecf8bf7a3ac428a824bef7cf9dfe610652f3b422
Endjrx <endijosueparodi66@gmail.com>
2025-11-15 14:36:08 -0500

    Error en argumentos de la funcion CharField en el modelo del veterinario

--- empl_veterinario/models.py
+++ empl_veterinario/models.py
@@ -5,7 +5,7 @@ from empleado.models import Empleado
 class Veterinario(models.Model):
     PK_IdVeterinario = models.OneToOneField(Empleado, on_delete=models.CASCADE, primary_key=True, db_column='PK_IdVeterinario')
     Especialidad = models.CharField(max_length=100, db_column='Especialidad')
-    Licencia = models.CharField(maxlength=50, db_column='Licencia')
+    Licencia = models.CharField(max_length=50, db_column='Licencia')
 
     class Meta:
         db_table = 'Veterinario'
