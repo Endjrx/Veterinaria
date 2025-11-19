d7a67c69b695b95b6151cc3d1d3e64ec2fc04a77
Endjrx <endijosueparodi66@gmail.com>
2025-11-19 03:32:35 -0500

    Solucion de problemas con nuevos nombres de atributos en los modelos

--- pagInicio/views.py
+++ pagInicio/views.py
@@ -7,7 +7,7 @@ from datetime import date
 @login_required(login_url='home-login')
 def home(request):
 
-    cita = Cita.objects.filter(Fecha=date.today(), Estado="Programada")
+    cita = Cita.objects.filter(fecha=date.today(), estado="Programada")
     pacientes = Cliente.objects.count()
     contador_citas = Cita.objects.count()
 
