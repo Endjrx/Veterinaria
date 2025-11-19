d8103e3453f83df3f754cd832840b347542baf85
Endjrx <endijosueparodi66@gmail.com>
2025-11-19 01:06:28 -0500

    Copia Seguridad PagInicio Lista de Citas hoy Funcionando

--- pagInicio/views.py
+++ pagInicio/views.py
@@ -7,10 +7,13 @@ from datetime import date
 @login_required(login_url='home-login')
 def home(request):
 
-    cita = Cita.objects.filter(Fecha=date.today()).filter(Estado="Programada").count()
+    cita = Cita.objects.filter(Fecha=date.today(), Estado="Programada")
     pacientes = Cliente.objects.count()
+    contador_citas = Cita.objects.count()
 
     return render(request, 'pagInicio/vet_dashboard.html', {
-        'total_citas': cita,
-        'total_clientes_activos': pacientes
+        'total_citas_hoy': cita.count(),
+        'total_clientes_activos': pacientes,
+        'total_citas': contador_citas,
+        'citas': cita
     })
