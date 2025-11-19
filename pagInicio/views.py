9ca96660685eb600b26ea5963944a2bca3671b67
Endjrx <endijosueparodi66@gmail.com>
2025-11-18 19:57:04 -0500

    Dashboard respondiendo a entiendades, copia respaldo

--- pagInicio/views.py
+++ pagInicio/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from cita.models import Cita
+from cliente.models import Cliente
 
 @login_required(login_url='home-login')
 def home(request):
-    return render(request, 'pagInicio/vet_dashboard.html')
-
+    cita = Cita.objects.filter(Estado="Programada").count() 
+    pacientes = Cliente.objects.count()
+    return render(request, 'pagInicio/vet_dashboard.html', {
+        'total_citas': cita,
+        'total_clientes_activos': pacientes
+    })
