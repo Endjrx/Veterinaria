086db5868f6923985798c1dba400eb6fa2700d91
Endjrx <endijosueparodi66@gmail.com>
2025-11-18 21:31:19 -0500

    Copia Seguridad PagInicio antes de eliminar recuadros de estadisticas

--- pagInicio/views.py
+++ pagInicio/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from cita.models import Cita
 from cliente.models import Cliente
+from datetime import date
 
 @login_required(login_url='home-login')
 def home(request):
-    cita = Cita.objects.filter(Estado="Programada").count() 
+
+    cita = Cita.objects.filter(Fecha=date.today()).filter(Estado="Programada").count()
     pacientes = Cliente.objects.count()
+
     return render(request, 'pagInicio/vet_dashboard.html', {
         'total_citas': cita,
         'total_clientes_activos': pacientes
