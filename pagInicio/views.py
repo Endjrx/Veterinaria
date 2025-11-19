de35ce96c3c709b53b8075d2dc62ec49d8c1e311
Endjrx <endijosueparodi66@gmail.com>
2025-11-19 04:16:10 -0500

    Gestion de nuevos estados

--- pagInicio/views.py
+++ pagInicio/views.py
@@ -7,7 +7,7 @@ from datetime import date
 @login_required(login_url='home-login')
 def home(request):
 
-    cita = Cita.objects.filter(fecha=date.today(), estado="Programada")
+    cita = Cita.objects.filter(fecha=date.today())
     pacientes = Cliente.objects.count()
     contador_citas = Cita.objects.count()
 
