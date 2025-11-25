438c02a20c74c0588c593a4e5c2adbc9b71a4dc3
Endjrx <endijosueparodi66@gmail.com>
2025-11-25 09:35:05 -0500

    Cambiar de relacion en factura del campo administrador_id que apunte y se relacione con los usuarios de auth_user django

--- factura/models.py
+++ factura/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from cliente.models import Cliente
 from administrador.models import Administrador
 from cita.models import Cita
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Factura(models.Model):
@@ -11,7 +12,7 @@ class Factura(models.Model):
     monto_total = models.DecimalField(max_digits=10, decimal_places=2, db_column='monto_total')
     estado_pago = models.CharField(max_length=50, db_column='estado_pago')
     cita_id = models.ForeignKey(Cita, on_delete=models.CASCADE, db_column='cita_id')
-    administrador_id = models.ForeignKey(Administrador, on_delete=models.CASCADE, db_column='administrador_id')
+    administrador_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='administrador_id')
     cliente_id = models.ForeignKey(Cliente, on_delete=models.CASCADE, db_column='cliente_id')
 
     class Meta:
